316aa31fd64a7a59403759a4363505a76b86f223
kaleem <makanari.android@gmail.com>
2024-03-29 14:34:41 +0530

    reading sms

--- app/src/main/java/com/ansari/myapplication/MainActivity.kt
+++ app/src/main/java/com/ansari/myapplication/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.ansari.myapplication
 
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var filter = IntentFilter("android.intent.action.AIRPLANE_MODE")
+        var filter = IntentFilter("android.provider.Telephony.SMS_RECEIVED")
         var myReceiver = MyReceiver()
         registerReceiver(myReceiver, filter )
+
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.RECEIVE_SMS)== PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"PERMISSION GRANTED",Toast.LENGTH_SHORT).show()
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECEIVE_SMS),124)
+        }
+
 
     }
 }
