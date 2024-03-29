d6cf6015a77c68f80815a2f65665125097777310
kaleem <makanari.android@gmail.com>
2024-03-29 12:31:04 +0530

    broadcast

--- app/src/main/java/com/ansari/myapplication/MainActivity.kt
+++ app/src/main/java/com/ansari/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ansari.myapplication
 
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var filter = IntentFilter("android.intent.action.AIRPLANE_MODE")
+        var myReceiver = MyReceiver()
+        registerReceiver(myReceiver, filter )
+
     }
 }
